1cff243eb2d522c313aa3303d9d395d821cbda77
Pawelpaprzycki <121885732+Pawelpaprzycki@users.noreply.github.com>
2023-03-27 17:30:16 +0200

    Add files via upload
    Web scraper made to work with justjoin.it. Scrapes job offers and saves them in JSON format in a specified location.
    To use, execute the script.

--- template.py
+++ template.py
@@ -1,1 +1,18 @@
-
+data = {
+    "title": "",
+    "street": "",
+    "country_code": "",
+    "address_text": "",
+    "marker_icon": "",
+    "workplace_type": "",
+    "company_name": "",
+    "company_url": "",
+    "company_size": "",
+    "experience_level": "",
+    "id": "",
+    "employment_types": "",
+    "skills": "",
+    "remote": "",
+    "multilocation": ""
+}
+x
